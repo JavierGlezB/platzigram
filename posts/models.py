0e2123768381f6938d1a2ee07c649057a3922e1c
JavierGlezB <xvrgonzalez21@gmail.com>
2020-01-11 14:04:52 -0600

    deleting not optimized user model

--- posts/models.py
+++ posts/models.py
@@ -3,26 +3,26 @@
 
 from django.db import models
 
-class User(models.Model):
+# class User(models.Model):
 
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
 
-    first_name = models.CharField(max_length=100)
-    last_name =  models.CharField(max_length=100)
+#     first_name = models.CharField(max_length=100)
+#     last_name =  models.CharField(max_length=100)
 
-    is_admin = models.BooleanField(default=False)
-    bio = models.TextField(blank=True)
+#     is_admin = models.BooleanField(default=False)
+#     bio = models.TextField(blank=True)
     
-    birthdate = models.DateField(blank=True, null=True)
-    created =  models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+#     birthdate = models.DateField(blank=True, null=True)
+#     created =  models.DateTimeField(auto_now_add=True)
+#     modified = models.DateTimeField(auto_now=True)
 
-    country = models.CharField(max_length=100, default='México')
-    city = models.CharField(max_length=100, default='')
+#     country = models.CharField(max_length=100, default='México')
+#     city = models.CharField(max_length=100, default='')
 
-    def __str__(self):
-        """return email"""
-        return self.email
+#     def __str__(self):
+#         """return email"""
+#         return self.email
 
 
