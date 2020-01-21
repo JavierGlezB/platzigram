29cce12de133154f9c537c23285925492c615d3d
JavierGlezB <xvrgonzalez21@gmail.com>
2020-01-21 11:08:27 -0600

    signup and profile middleware

--- users/views.py
+++ users/views.py
@@ -4,10 +4,17 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.utils import IntegrityError
 import pdb
+from django.contrib.auth.models import User
+from users.models import Profile
 
 
 # Create your views here.
+
+def update_profile(request):
+    """update user profile"""
+    return render(request, 'users/update_profile.html')
 
 
 def login_view(request):
@@ -31,3 +38,31 @@ def logout_view(request):
     return redirect('login')
 
 
+
+def signup_view(request):
+    """Sign up view."""
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
